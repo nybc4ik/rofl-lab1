59b009fc0497df7b46cb829b9232f67b96fa19d0
ShilovaMaria <108538231+ShilovaMaria@users.noreply.github.com>
2024-10-01 18:32:29 +0300

    Убрала все ненужное с предыдущей лабы

--- lab1_interpreter.py
+++ lab1_interpreter.py
@@ -1,7 +1,7 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 import re
 from sympy import simplify as sympy_simplify, expand, Symbol
-from z3 import *
+#from z3 import *
 
 '''
   The simplify_expression function simplifies expressions. 
@@ -48,148 +48,94 @@ def to_prefix_notation(expression, index):
     prefix_expression += ')'
     return prefix_expression, variables_set
 
+#Пока что временно ввод всего чего хотим вручную
 
-'''
-def strInterp(i):
-    s1=str(i)
-    s="a"+s1+"*x+"+"b"+s1
-    return s
-'''
+#trs_variables=["x","y"]
+trs_variables = str(input("variables = ")).split(',')
+#trs_rules=["f(x)=a", "g(x)=f(f(x))", "u(x,y)=c(g(x),f(y))"]
+trs_rules=[]
+rules= input("input trs rules\n")
+while rules!= "":
+    trs_rules.append(rules)
+    rules=input()
+#grammar_rules=["a=1", "f(x)=x**2+2*x+1", "g(x)=x**3", "u(x,y)=x*y", "c(x,y)=x+y"]
+grammar_rules=[]
+rules= input("input grammar rules\n")
+while rules!= "":
+    grammar_rules.append(rules)
+    rules=input()
+constructors=[]
+for i in range(len(grammar_rules)):
+    constructors.append(grammar_rules[i][0])
 
-strings = []
-strdiv = []
-start = []
-end = []
-alf = ""
-a = []
-b = []
-i1 = 0
-div = 0
-strings.append(str(input()))
-while (strings[div] != ""):
-    strings[div] = strings[div].replace(" ", "")
-    string = strings[div]
-    f = 0
-    start = []
-    end = []
-    for i in range(len(string)):
-        if string[i] == '-' or string[i] == '>':
-            f = 1
-        elif alf.find(string[i]) == -1:
-            if f == 0:
-                start.append(len(alf))
-            else:
-                end.append(len(alf))
-            alf = alf + string[i]
-        else:
-            if f == 0:
-                start.append(alf.find(string[i]))
-            else:
-                end.append(alf.find(string[i]))
+#Я забыла как это действие с залезанием в скобки называется по нормальному, поэтому пока   будет называться podmena
+#Потом заменю
+def podmena(string):
+    
+    i=0
+    if not (string[i] in constructors):
+        return trs_variables[trs_variables.index(string[i])]
+    con=constructors.index(string[i])
+    
+    if grammar_rules[con][1]=='=':
+        return grammar_rules[con][2:]
+    n=grammar_rules[con].count(',')+1
+    rule=grammar_rules[con][2*n+3:]
+    i=2
+    s=""
+    k=0 
+    while k!=n and i<len(string):
+        if string[i]==',':
+            rule=rule.replace(grammar_rules[con][2*k+2],"("+podmena(s)+")")
+            k+=1
+            s=""
+            i+=1
+        s+=string[i]
+        i+=1
+    s=s[:-1]
+    rule=rule.replace(grammar_rules[con][2*k+2],"("+podmena(s)+")")
+    return rule
 
-    ls = len(start)
-    le = len(end)
-    d1 = i1
-    a.append(" a" + str(start[0]))
-    a.append("b" + str(start[ls - 1]))
-    mul = ""
-    br = ""
 
-    # here we make strings
 
-    startString = " + " + a[i1 + 1]
-    startString1 = ""
-    for i in range(ls - 1, 0, -1):
-        mul = "(* a" + str(start[i]) + mul
-        startString1 = "a" + str(start[i]) + "*" + startString1
-        startString = " + " + startString1 + "b" + str(start[i - 1]) + startString
-        br = ")" * (ls - i)
-        a[i1 + 1] = "(+ " + mul + " b" + str(start[i - 1]) + br + " " + a[i1 + 1] + ")"
-    startString = startString1 + "a" + str(start[0]) + "*x" + startString
-    a[i1] = mul + a[i1] + br
-
-    i1 = d1
-    b.append(" a" + str(end[0]))
-    b.append("b" + str(end[le - 1]))
-    mul = ""
-    br = ""
-
-    endString = " + " + b[i1 + 1]
-    endString1 = ""
-    for i in range(le - 1, 0, -1):
-        mul = "(* a" + str(end[i]) + mul
-        endString1 = "a" + str(end[i]) + "*" + endString1
-        endString = " + " + endString1 + "b" + str(end[i - 1]) + endString
-        br = ")" * (le - i)
-        b[i1 + 1] = "(+ " + mul + " b" + str(end[i - 1]) + br + " " + b[i1 + 1] + ")"
-    endString = endString1 + "a" + str(end[0]) + "*x" + endString
-    b[i1] = mul + b[i1] + br
-
-    i1 += 2
-    strdiv.append(i1)
-    div += 1
-
-    strings.append(str(input()))
-    print(startString)
-    print(endString)
-
-# here we start working
+#-----------------------------------------------------------------------------
+start_expressions=[]
+end_expressions=[]
 with open('lab1.txt', 'w') as f:
-    for i in range(len(alf)):
-        f.write("(declare-fun a" + str(i) + " () Int)\n")
-        f.write("(declare-fun b" + str(i) + " () Int)\n")
-        # f.write("(declare-fun c"+str(i)+" () Int)\n")
-        # f.write("(declare-fun d"+str(i)+" () Int)\n")
-    for i in range(len(alf)):
-        f.write("(assert (>= a" + str(i) + " 0))\n")
-        f.write("(assert (>= b" + str(i) + " 0))\n")
-        # f.write("(assert (> c"+str(i)+" 0))\n")
-        # f.write("(assert (>= d"+str(i)+" 0))\n")
-
-    n1 = 0
-    for div1 in range(div):
-        mainString = "(assert (or (and "
-        xs1 = ""
-        xs2 = ""
-        nxs1 = ""
-        nxs2 = ""
-        if div1 > 0:
-            n = (strdiv[div1] - strdiv[div1 - 1]) // 2 - 1
-        else:
-            n = strdiv[div1] // 2 - 1
-        n2 = n1 + n
-        s2 = ""
-        for i in range(n1, n2 + 1):
-            # print(i,n1,n2)
-            s1 = "(> " + a[i] + " " + b[i] + ")"
-            s2 = "(>= " + a[i] + " " + b[i] + ")"
-            if i == n2:
-                xs1 = xs1 + s1
-                xs2 = xs2 + s2
-            else:
-                xs1 = xs1 + "(or " + s1 + " (and " + s2 + " "
-                xs2 = xs2 + "(and " + s2 + " "
-            s1 = "(> " + a[i + n + 1] + " " + b[i + n + 1] + ")"
-            s2 = "(>= " + a[i + n + 1] + " " + b[i + n + 1] + ")"
-            if i == n2:
-                nxs1 = nxs1 + s1
-                nxs2 = nxs2 + s2
-            else:
-                nxs1 = nxs1 + "(or " + s1 + " (and " + s2 + " "
-                nxs2 = nxs2 + "(and " + s2 + " "
-
-        for i in range(n):
-            xs1 = xs1 + "))"
-            xs2 = xs2 + ")"
-            nxs1 = nxs1 + "))"
-            nxs2 = nxs2 + ")"
-
-        mainString = mainString + xs1 + " " + nxs2 + ") (and " + xs2 + " " + nxs1 + ")))\n"
-        f.write(mainString)
-        n1 = strdiv[div1]
-
+    for cr in range(len(trs_rules)):
+        trs_rules[cr] = trs_rules[cr].replace(" ", "")
+        
+        start = trs_rules[cr][:(trs_rules[cr].find('='))]
+        end = trs_rules[cr][(trs_rules[cr].find('=')+1):]
+        
+        start=simplify_expression(podmena(start))
+        end=simplify_expression(podmena(end))
+        
+        
+        start_expression, start_variables_set = to_prefix_notation(start, str(cr))
+        end_expression, end_variables_set = to_prefix_notation(end, str(cr))
+        variables_set=start_variables_set|end_variables_set
+        start_expressions.append(start_expression)
+        end_expressions.append(end_expression)
+        for v in variables_set:
+            f.write("(declare-fun " + v + " () Int)\n")
+        #for v in variables_set:
+        #    f.write("(assert (>= " + v + " 0))\n")
+    for cr in range(len(trs_rules)): 
+        f.write("(assert (<= " + start_expressions[cr] + " " + end_expressions[cr] + "))\n")
     f.write("(check-sat)\n")
     f.write("(get-model)")
+
+
+''''
+expression = input("Enter expression: ")
+simplified_expression = simplify_expression(expression)
+print("Simplified expression:", simplified_expression)
+
+prefix_expression, variables_set = to_prefix_notation(simplified_expression, '0')
+print("Prefix notation:", prefix_expression)
+print("Set of variables: ", variables_set)
+'''
 
 with open('lab1.txt', 'r') as f:
     smt_code = f.read()
@@ -207,20 +153,5 @@ elif result == unsat:
 else:
     print("Unknown")
 
-expression = input("Enter expression: ")
-simplified_expression = simplify_expression(expression)
-print("Simplified expression:", simplified_expression)
 
-prefix_expression, variables_set = to_prefix_notation(simplified_expression, '0')
-print("Prefix notation:", prefix_expression)
-print("Set of variables: ", variables_set)
-
-'''
-anotherstartString="x"
-anotherendString="x"
-for i in start:
-    anotherstartString=anotherstartString.replace("x", "("+strInterp(i)+")")
-for i in end:
-    anotherendString=anotherendString.replace("x", "("+strInterp(i)+")")
-'''
 
